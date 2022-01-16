9c3fb45f52770317e315df0bc4c04c13752b91be
tomsmolin <tomsmo016@gmail.com>
2022-01-16 13:44:39 +0000

    challenge 0, not tested

--- HW_4/malloc_3.cpp
+++ HW_4/malloc_3.cpp
@@ -1,7 +1,11 @@
 #include <cstring>
 #include <unistd.h>
-#define MAX 100000000
+
+// max bin hold less than (128 + 1) * 1024 = 132096
+#define MAX 132095
 #define MIN 0
+#define KB  1024
+
 
 // Classes
 class MallocMetaData {
@@ -10,35 +14,45 @@ public:
     bool is_free;
     MallocMetaData* next;
     MallocMetaData* prev;
+    // bin can be computed using size / KB (=1024)
 };
 
+class Bin {
+public:
+    // Data members
+    MallocMetaData* head;
+
+    // Methods
+    Bin() : head(nullptr) {}
+};
+
+
 // Static Variables
+static Bin bins[128];
 static size_t num_free_blocks = 0;
 static size_t num_free_bytes = 0;
 static size_t num_allocated_blocks = 0;
 static size_t num_allocated_bytes = 0;
 
-// Global pointer to the head of the list
-MallocMetaData* head = nullptr;
 
 // Functions
 void* smalloc(size_t size) {
     if((size == MIN) || (size > MAX)) {
         return NULL;
     }
+    int bin_index = size / KB;
     //Search for free block
-    MallocMetaData* tmp = head; // iterator
+    MallocMetaData* tmp = bins[bin_index].head; // iterator
     while (tmp != nullptr) {
         if (tmp->is_free && tmp->size >= size) { // allocate the first free block that fits
             tmp->is_free = false;
-            // handles static variables
             num_free_blocks -= 1;
             num_free_bytes -= tmp->size;
             return (void*)((char*)tmp + sizeof(MallocMetaData)); //return the block after the meta data
         }
         tmp = tmp->next;
     }
-    //No free blocks
+    //No free blocks in desired bin
     void* result = sbrk(size+sizeof(MallocMetaData));
     if (result == (void*)(-1)) {
         return NULL;
@@ -47,21 +61,57 @@ void* smalloc(size_t size) {
     meta_data->size = size;
     meta_data->is_free = false;
     meta_data->next = nullptr;
+    meta_data->prev = nullptr;
     // handles static variables
     num_allocated_blocks += 1;
     num_allocated_bytes += size;
-    //if list was empty
-    if(head == nullptr) {
-        head = meta_data;
-        head->prev = nullptr;
+    //if bin was empty
+    if(bins[bin_index].head == nullptr) {
+        bins[bin_index].head = meta_data;
     }
     else {
-        MallocMetaData* temp = head; //iterator
-        while(temp->next!= nullptr) {
+        //iterator
+        MallocMetaData* temp = bins[bin_index].head;
+        MallocMetaData* pre_temp = nullptr;
+
+        // edge case 1: if new MetaData is new head
+        if (bins[bin_index].head->size >= size) {
+            meta_data->next = bins[bin_index].head;
+            bins[bin_index].head->prev = meta_data;
+            bins[bin_index].head = meta_data;
+            return (void*)((char*)meta_data + sizeof(MallocMetaData));
+        }
+        // finding a place between lesser and greater values of size
+        while(temp != nullptr) {
+            pre_temp = temp;
+            if(temp->size <= size && temp->next != NULL && temp->next->size >= size) {
+                meta_data->next = temp->next;
+                temp->next = meta_data;
+                meta_data->prev = temp;
+                meta_data->next->prev = meta_data;
+                return (void*)((char*)meta_data + sizeof(MallocMetaData));
+            }
             temp = temp->next;
-        } // find last in the list
-        temp->next = meta_data;
-        meta_data->prev = temp;
+        }
+//         we reached an edge - no place in between - last MetaData in list
+//        if (pre_temp->size > size) {
+//            if (pre_temp == bins[bin_index].head) {
+//                bins[bin_index].head = meta_data;
+//                meta_data->next = pre_temp;
+//                pre_temp->prev = meta_data;
+//            }
+//            else {
+//                meta_data->prev = pre_temp->prev;
+//                pre_temp->prev->next = meta_data;
+//                meta_data->next = pre_temp;
+//                pre_temp->prev = meta_data;
+//            }
+//        }
+
+        // edge case 2: if new MetaData is new tail
+        meta_data->next = pre_temp->next;
+        pre_temp->next = meta_data;
+        meta_data->prev = pre_temp;
     }
     return (void*)((char*)meta_data + sizeof(MallocMetaData));
 }
